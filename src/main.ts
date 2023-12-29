8066791e27561bf27652b7e2e1e7b4a0e75f947f
kkulebaev <konstantinkulebaev@gmail.com>
2023-12-29 13:30:17 +0300

    refactor: add reset.css

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import './style.css'
+import './assets/style/style.css'
 import App from './App.vue'
 import { initMirageServer } from './server/server'
 import { ElLoading } from 'element-plus'
